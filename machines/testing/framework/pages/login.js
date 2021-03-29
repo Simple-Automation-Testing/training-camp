ff37e1b7d226118c8a102c21664dcb29a4a0924a
andrei nalivaika <andreinalivaika@andreis-MacBook-Pro-2.local>
2021-03-29 21:51:16 +0300

    made function arguments an object

--- machines/testing/framework/pages/login.js
+++ machines/testing/framework/pages/login.js
@@ -1,48 +1,53 @@
 class LoginPage {
-    static LOGIN_URL = `http://localhost:8081/`;
-    static LOGIN_FORM_USER_NAME_FIELD = `//input[@placeholder="Ім'я користувача"]`;
-    static LOGIN_FORM_PASSWORD_FIELD = `//input[@placeholder="пароль"]`;
-    static LOGIN_FORM_SIGN_IN_BUTTON = `(//button[text()="Увійти"])[2]`;
-    static MENU_SIGN_IN = `(//button[text()="Увійти"])[1]`
-    static MENU_REGISTER = `//button[text()="Зареєструватися"]`;
-    static CHECKIN_FORM_USERNAME_FIELD =  `//input[@placeholder="Ім'я користувача"]`;
-    static CHECKIN_FORM_NAME_FIELD = `//input[@placeholder="Ім'я"]`;
-    static CHECKIN_FORM_EMAIL_FIELD = `//input[@placeholder="Імейл"]`;
-    static CHECKIN_FORM_PASSWORD_FIELD = `//input[@placeholder="пароль"]`;
-    static CHECKIN_FORM_REGISTER_BUTTON = `(//button[text()="Зареєструватися"])[2]`;
+	static LOGIN_URL = `http://localhost:8081/`;
+	static LOGIN_FORM_USER_NAME_FIELD = `//input[@placeholder="Ім'я користувача"]`;
+	static LOGIN_FORM_PASSWORD_FIELD = `//input[@placeholder="пароль"]`;
+	static LOGIN_FORM_SIGN_IN_BUTTON = `(//button[text()="Увійти"])[2]`;
+	static MENU_SIGN_IN = `(//button[text()="Увійти"])[1]`;
+	static MENU_REGISTER = `//button[text()="Зареєструватися"]`;
+	static CHECKIN_FORM_USERNAME_FIELD = `//input[@placeholder="Ім'я користувача"]`;
+	static CHECKIN_FORM_NAME_FIELD = `//input[@placeholder="Ім'я"]`;
+	static CHECKIN_FORM_EMAIL_FIELD = `//input[@placeholder="Імейл"]`;
+	static CHECKIN_FORM_PASSWORD_FIELD = `//input[@placeholder="пароль"]`;
+	static CHECKIN_FORM_REGISTER_BUTTON = `(//button[text()="Зареєструватися"])[2]`;
 
-    constructor(page) {
-        this.page = page;
-    }
+	constructor(page) {
+		this.page = page;
+	}
 
-    async switchTo(modalName) {
-        await this.page.goto(LoginPage.LOGIN_URL);
-        switch (modalName) {
-            case 'checkin':
-                await (await this.page.$(LoginPage.MENU_REGISTER)).click();
-                break;
-            case 'login':
-                await (await this.page.$(LoginPage.MENU_SIGN_IN)).click();
-                break;
-            default:
-                console.log(`Sorry, no such modal: ${modalName}`);
-        }
-    }
+	async switchTo(modalName) {
+		await this.page.goto(LoginPage.LOGIN_URL);
+		switch (modalName) {
+			case "checkin":
+				await (await this.page.$(LoginPage.MENU_REGISTER)).click();
+				break;
+			case "login":
+				await (await this.page.$(LoginPage.MENU_SIGN_IN)).click();
+				break;
+			default:
+				console.log(`Sorry, no such modal: ${modalName}`);
+		}
+	}
 
-    async checkIn(username, name, email, password) {
-        await this.page.fill(LoginPage.CHECKIN_FORM_USERNAME_FIELD, username);
-        await this.page.fill(LoginPage.CHECKIN_FORM_NAME_FIELD, name);
-        await this.page.fill(LoginPage.CHECKIN_FORM_EMAIL_FIELD, email);
-        await this.page.fill(LoginPage.CHECKIN_FORM_PASSWORD_FIELD, password);
-        await (await this.page.$(LoginPage.CHECKIN_FORM_REGISTER_BUTTON)).click();
-    }
+	async checkIn({ options }) {
+		await this.page.fill(
+			LoginPage.CHECKIN_FORM_USERNAME_FIELD,
+			options.username
+		);
+		await this.page.fill(LoginPage.CHECKIN_FORM_NAME_FIELD, options.name);
+		await this.page.fill(LoginPage.CHECKIN_FORM_EMAIL_FIELD, options.email);
+		await this.page.fill(
+			LoginPage.CHECKIN_FORM_PASSWORD_FIELD,
+			options.password
+		);
+		await (await this.page.$(LoginPage.CHECKIN_FORM_REGISTER_BUTTON)).click();
+	}
 
-    async login(name, password) {
-        await this.page.fill(LoginPage.LOGIN_FORM_USER_NAME_FIELD, name);
-        await this.page.fill(LoginPage.LOGIN_FORM_PASSWORD_FIELD, password);
-        await (await this.page.$(LoginPage.LOGIN_FORM_SIGN_IN_BUTTON)).click();
-    }
-
+	async login({ options }) {
+		await this.page.fill(LoginPage.LOGIN_FORM_USER_NAME_FIELD, options.name);
+		await this.page.fill(LoginPage.LOGIN_FORM_PASSWORD_FIELD, options.password);
+		await (await this.page.$(LoginPage.LOGIN_FORM_SIGN_IN_BUTTON)).click();
+	}
 }
 
 module.exports = { LoginPage };
--- machines/testing/index.spec.js
+++ machines/testing/index.spec.js
@@ -1,42 +1,48 @@
-const {chromium, firefox, webkit} = require('playwright');
-const {LoginPage, TablesPage} = require('./framework/pages/index');
-const {expect} = require('chai');
+const { chromium, firefox, webkit } = require("playwright");
+const { LoginPage, TablesPage } = require("./framework/pages/index");
+const { expect } = require("chai");
 
-describe('Login file', () => {
+describe("Login file", () => {
+	let browser = null;
+	let context = null;
+	let page = null;
 
-    let browser = null;
-    let context = null;
-    let page = null;
+	beforeEach(async () => {
+		browser = await chromium.launch({ headless: false, slowMo: 50 });
+		context = await browser.newContext();
+		page = await context.newPage();
+	});
 
-    beforeEach( async () => {
-        browser = await chromium.launch({ headless: false, slowMo: 50 });
-        context = await browser.newContext();
-        page = await context.newPage();
-    });
+	afterEach(async () => {
+		await browser.close();
+	});
 
-    afterEach(async () => {
-        await browser.close();
-    });
+	describe("Check-in in the app", async () => {
+		it("should check-in a new user and redirect him to Tables page", async () => {
+			const loginPage = new LoginPage(page);
+			await loginPage.switchTo("checkin");
+			await loginPage.checkIn({
+				username: "andrei",
+				name: "andrei",
+				email: "andrei",
+				password: "andrei",
+			});
 
-    describe('Check-in in the app', async () => {
-        it('should check-in a new user and redirect him to Tables page', async () => {
-            const loginPage = new LoginPage(page);
-            await loginPage.switchTo('checkin');
-            await loginPage.checkIn('andrei', 'andrei', 'andrei', 'andrei');
+			expect(await page.textContent(TablesPage.TITLE)).to.equal(
+				"Таблиці, Привіт andrei"
+			);
+		});
+	});
 
-            expect(await page.textContent(TablesPage.TITLE)).to.equal('Таблиці, Привіт andrei');
-        });
-    });
+	describe("Login to the app", async () => {
+		it("should login an exisiting user and redirect him to Tables page", async () => {
+			const loginPage = new LoginPage(page);
+			await loginPage.switchTo("login");
+			await loginPage.login({ name: "admin", password: "admin" });
 
-    describe('Login to the app', async () => {
-        it('should login an exisiting user and redirect him to Tables page', async () => {
-            const loginPage = new LoginPage(page);
-            await loginPage.switchTo('login');
-            await loginPage.login('admin', 'admin');
-
-            expect(await page.textContent(TablesPage.TITLE)).to.equal('Таблиці, Привіт admin');
-        });
-    });
-
+			expect(await page.textContent(TablesPage.TITLE)).to.equal(
+				"Таблиці, Привіт admin"
+			);
+		});
+	});
 });
-
