619a18a89d44c13341dce83dea4708ba04aeee09
andrei nalivaika <andreinalivaika@andreis-MacBook-Pro-2.local>
2021-04-23 16:02:38 +0300

    add decorators for some methods

--- machines/testing/lib/base/elements/input.js
+++ machines/testing/lib/base/elements/input.js
@@ -1,4 +1,5 @@
 const { Element } = require("./element");
+const { step } = require("../../report");
 
 class Input extends Element {
 	constructor(selector, name, page) {
@@ -17,11 +18,14 @@ class Input extends Element {
 	 * @public
 	 * @param {string} [options = {}] keys - a text to type into a rootElement, options - optional parameter
 	 */
+
+	@step((name) => `${name} executes sendKeys`)
 	async sendKeys(...keys) {
 		await this._initRootElement();
 		await this.__rootElement.type(...keys);
 	}
 
+	@step((name) => `${name} executes isCheckboxChecked`)
 	async isCheckboxChecked() {
 		await this._initRootElement();
 		return await this.__rootElement.isChecked();
