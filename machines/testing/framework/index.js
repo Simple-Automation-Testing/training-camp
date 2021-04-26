6da1335e777e7b3b5bdeea584afe029fd241c89d
andrei nalivaika <andreinalivaika@andreis-MacBook-Pro-2.local>
2021-04-26 21:48:35 +0300

    changed assertion on assertior

--- machines/testing/framework/index.js
+++ machines/testing/framework/index.js
@@ -1,10 +1,11 @@
-const { allure } = require("allure-mocha/runtime");
+//const { allure } = require("allure-mocha/runtime");
 const { ContentType } = require("allure-js-commons");
 const { browserInterface } = require("./../lib/base/browser/index");
 const { expect, initStepDeclarator } = require("assertior");
 const { wrappedIt, wrappedBeforeEach, wrappedAfterEach } = require("./../lib/runner/runner");
 
 function allureStep(stepAssertionName, error, current, expected) {
+	const { allure } = require("allure-mocha/runtime");
 	const step = allure.startStep(stepAssertionName);
 	allure.attachment("Expected value", JSON.stringify(expected, null, 2), ContentType.JSON);
 	allure.attachment("Current value", JSON.stringify(current, null, 2), ContentType.JSON);
--- machines/testing/specs/index.spec.js
+++ machines/testing/specs/index.spec.js
@@ -1,7 +1,6 @@
 const { provider } = require("./../framework/index");
 const { browser } = provider;
 const { LoginPage, TablesPage, AdminPage } = require("./../framework/pages/index");
-//const { expect } = require("chai");
 const { expect } = provider.packages;
 const { it, beforeEach, afterEach } = provider.testRunner;
 
@@ -10,23 +9,18 @@ describe("Login file", () => {
 	let context = null;
 	let page = null;
 
-	//@wrappedBeforeEach(name => `${name} executes beforeEach`)
-	beforeEach(async () => {
+	beforeEach("Executes beforeEach", async () => {
 		browserInstance = await browser.initBrowser("chromium", { headless: false, slowMo: 50 });
 		context = await browser.createNewContext(browserInstance);
 		await browser.setStorage(context);
 		page = await browser.createNewPage(context);
 	});
 
-	//@wrappedAfterEach(name => `${name} executes afterEach`)
-	afterEach(async () => {
-		await browser.createScreenshot();
-		// console.log(await browserInterface.getCurrentUrl());
+	afterEach("Executes afterEach", async () => {
 		await browser.closeBrowser(browserInstance);
 	});
 
 	describe("Check-in to the app as new user", async () => {
-		//@wrappedIt(name => `${name} executes it`);
 		it("should check-in a new user and redirect him to Tables page", async () => {
 			const loginPage = new LoginPage(page);
 			await loginPage.open();
@@ -34,7 +28,7 @@ describe("Login file", () => {
 			await loginPage.checkIn({ username: "anav", name: "andrei", email: "anav@gmail.com", password: "andrei" });
 
 			const tablePage = new TablesPage(page);
-			expect(await tablePage.getTitle()).to.equal("Таблиці, Привіт anav");
+			expect(await tablePage.getTitle()).toEqual("Таблиці, Привіт anav");
 		});
 	});
 
@@ -45,7 +39,7 @@ describe("Login file", () => {
 			await loginPage.login({ name: "admin", password: "admin" });
 
 			const tablePage = new TablesPage(page);
-			expect(await tablePage.getTitle()).to.equal("Таблиці, Привіт admin");
+			expect(await tablePage.getTitle()).toEqual("Таблиці, Привіт admin");
 		});
 	});
 
@@ -59,7 +53,7 @@ describe("Login file", () => {
 			await tablePage.toAdminPage();
 
 			const adminPage = new AdminPage(page);
-			expect(await adminPage.getTitle()).to.equal("Кабінет адміністратора, Привіт admin");
+			expect(await adminPage.getTitle()).toEqual("Кабінет адміністратора, Привіт admin");
 		});
 	});
 
@@ -74,7 +68,7 @@ describe("Login file", () => {
 
 			const adminPage = new AdminPage(page);
 			await adminPage.createUser({ username: "anav1", name: "andrei", email: "anav1@gmail.com", password: "andrei1" });
-			expect(await adminPage.isUserExistInArray("anav1")).to.be.true;
+			expect(await adminPage.isUserExistInArray("anav1")).toEqual(true);
 		});
 
 		it("should create new user with role Admin", async () => {
@@ -87,8 +81,8 @@ describe("Login file", () => {
 
 			const adminPage = new AdminPage(page);
 			await adminPage.createUser({ username: "anav2", name: "andrei", email: "anav2@gmail.com", password: "andrei2", isAdmin: true });
-			expect(await adminPage.isUserExistInArray("anav2")).to.be.true;
-			expect(await adminPage.isUserAdmin("anav3")).to.be.true;
+			expect(await adminPage.isUserExistInArray("anav2")).toEqual(false);
+			expect(await adminPage.isUserAdmin("anav2")).toEqual(false);
 		});
 	});
 });
