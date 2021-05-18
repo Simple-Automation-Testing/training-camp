55ea4167112471f64672b2a5f108f78d791f5bf0
andrei nalivaika <andreinalivaika@andreis-MacBook-Pro-2.local>
2021-05-18 21:16:51 +0300

    devided specs to separate files

--- machines/testing/specs/index.spec.js
+++ machines/testing/specs/index.spec.js
@@ -2,27 +2,15 @@ const { provider } = require("./../framework/index");
 const { browser } = provider;
 const { LoginPage, TablesPage, AdminPage } = require("./../framework/pages/index");
 const { expect } = provider.packages;
-const { it, beforeEach, afterEach } = provider.testRunner;
+const { it, beforeEach, afterEach, beforeAll, afterAll } = provider.testRunner;
 
-describe("Login file", () => {
+describe("Login sute", () => {
 	beforeEach("Executes beforeEach", async () => {
 		await browser.initBrowser("chromium", { headless: false, slowMo: 50 });
 	});
 
 	afterEach("Executes afterEach", async () => {
 		await browser.closeBrowser();
-	});
-
-	describe("Check-in to the app as new user", async () => {
-		it("should check-in a new user and redirect him to Tables page", async () => {
-			const loginPage = new LoginPage();
-			await loginPage.open();
-			await loginPage.switchTo("checkin");
-			await loginPage.checkIn({ username: "anav", name: "andrei", email: "anav@gmail.com", password: "andrei" });
-
-			const tablePage = new TablesPage();
-			expect(await tablePage.getTitle()).toEqual("Таблиці, Привіт anav");
-		});
 	});
 
 	describe("Login to the app as exisitng user", async () => {
@@ -47,35 +35,6 @@ describe("Login file", () => {
 
 			const adminPage = new AdminPage();
 			expect(await adminPage.getTitle()).toEqual("Кабінет адміністратора, Привіт admin");
-		});
-	});
-
-	describe("Create new user via admin cabinet", () => {
-		it("should create new user with role User", async () => {
-			const loginPage = new LoginPage();
-			await loginPage.open();
-			await loginPage.login({ name: "admin", password: "admin" });
-
-			const tablePage = new TablesPage();
-			await tablePage.toAdminPage();
-
-			const adminPage = new AdminPage();
-			await adminPage.createUser({ username: "anav1", name: "andrei", email: "anav1@gmail.com", password: "andrei1" });
-			expect(await adminPage.isUserExistInArray("anav1")).toEqual(true);
-		});
-
-		it("should create new user with role Admin", async () => {
-			const loginPage = new LoginPage();
-			await loginPage.open();
-			await loginPage.login({ name: "admin", password: "admin" });
-
-			const tablePage = new TablesPage();
-			await tablePage.toAdminPage();
-
-			const adminPage = new AdminPage();
-			await adminPage.createUser({ username: "anav2", name: "andrei", email: "anav2@gmail.com", password: "andrei2", isAdmin: true });
-			expect(await adminPage.isUserExistInArray("anav2")).toEqual(false);
-			expect(await adminPage.isUserAdmin("anav2")).toEqual(false);
 		});
 	});
 });
